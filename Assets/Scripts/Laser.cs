7ff324470a0ca96ddb47c8ce91d6544ed3141eef
vahittm <154670042+vahittm@users.noreply.github.com>
2024-01-07 17:09:26 +0330

    Destroy laser powerup parent

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -16,6 +16,10 @@ public class Laser : MonoBehaviour
 
       if (transform.position.y>8f)
       {
+          if (transform.parent != null)
+          {
+              Destroy(transform.parent.gameObject);
+          }
           Destroy(this.gameObject);
       }    
     }
